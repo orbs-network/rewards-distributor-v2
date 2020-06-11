ef099779adc4b0a96d05a6bed6c39efa38e10377
Or Lavy <orlavyil@gmail.com>
2020-06-11 17:49:06 +0300

    Prettier

--- rewards-ui/src/App.tsx
+++ rewards-ui/src/App.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { sum } from 'rewards-v2/dist/src/sum';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { sum } from "rewards-v2/dist/src/sum";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The sum of 1 and 2 is { sum(1,2) }
-        </p>
+        <p>The sum of 1 and 2 is {sum(1, 2)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
