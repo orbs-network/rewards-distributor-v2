eca95704063b9363753eb410ac745eeed799fee3
Kirill Maksimov <kirill@orbs.com>
2020-08-12 17:05:01 +0300

    Always release to staging

--- .circleci/rewards-service/release-to-staging.sh
+++ .circleci/rewards-service/release-to-staging.sh
@@ -4,5 +4,10 @@ docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
 
 export VERSION=$(cat .version)
 
-docker tag orbsnetwork/rewards-service:$VERSION orbsnetworkstaging/rewards-service:$VERSION
 docker push orbsnetworkstaging/rewards-service:$VERSION
+
+if [[ $CIRCLE_BRANCH == "master" ]] ;
+then
+  docker tag orbsnetworkstaging/rewards-service:$VERSION orbsnetworkstaging/rewards-service:experimental
+  docker push orbsnetworkstaging/rewards-service:experimental
+fi
